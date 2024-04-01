3a021a6f83950852ada3bb1e617909f6bce4a9b7
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-04-01 16:17:21 -0500

    added readme and made changed to script and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,50 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+        const taskCard = $('<div>')
+          .addClass('card project-card draggable my-3')
+          .attr('data-project-id', project.id);
+        const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+        const cardBody = $('<div>').addClass('card-body');
+        const cardDescription = $('<p>').addClass('card-text').text(task.type);
+        const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+        const cardDeleteBtn = $('<button>')
+          .addClass('btn btn-danger delete')
+          .text('Delete')
+          .attr('data-project-id', project.id);
+        cardDeleteBtn.on('click', handleDeleteProject);
+      
 }
+if (project.dueDate && project.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSameorbefore(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  return taskCard;
+
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+        //     $('#skills-list').sortable({
+        //       placeholder: "sortable-placeholder",
+        //       forcePlaceholderSize: "true",
+        //     });
+        //   });
+        
 }
 
 // Todo: create a function to handle adding a new task
@@ -36,3 +74,103 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+$(function () {
+    $('#datepicker').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
+  
+  // Add interaction here
+  //
+//   $(function() {
+//     $('#skills-list').sortable({
+//       placeholder: "sortable-placeholder",
+//       forcePlaceholderSize: "true",
+//     });
+//   });
+
+
+// $(function(){
+//     $('#myForm').on('submit', function(e){
+//       e.preventDefault();
+//       $.post('http://www.somewhere.com/path/to/post', 
+//          $('#myForm').serialize(), 
+//          function(data, status, xhr){
+//            // do something here with response;
+//          });
+//     });
+// });
+// // Get the modal
+const modal = document.getElementById("#formModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("btn btn-success");
+
+// Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+
+//   const formEl = $('#guestbook-form');
+// const nameInputEl = $('#name-input');
+// const commentInputEl = $('#comment-input');
+// const guestBookDisplayEl = $('#guest-book-display');
+
+// const printGuestData = function (name, comment) {
+//   const cardColumnEl = $('<div>');
+//   cardColumnEl.addClass('col-12 col-sm-4 col-md-3');
+
+//   const cardEl = $('<div>');
+//   // Add a class of .custom-card
+//   cardEl.addClass('card h-100 custom-card');
+//   cardEl.appendTo(cardColumnEl);
+
+//   // Add a class of .custom-card-header
+//   const cardName = $('<h5>').addClass('card-header custom-card-header').text(name);
+//   cardName.appendTo(cardEl);
+
+//   const cardBodyEl = $('<div>');
+//   cardBodyEl.addClass('card-body');
+//   cardBodyEl.appendTo(cardEl);
+
+//   const cardComment = $('<p>').addClass('card-text').text(comment);
+//   cardComment.appendTo(cardBodyEl);
+
+//   guestBookDisplayEl.append(cardColumnEl);
+// };
+
+// const handleFormSubmit = function (event) {
+//   event.preventDefault();
+
+//   const nameInput = nameInputEl.val();
+//   const commentInput = commentInputEl.val();
+
+//   if (!nameInput || !commentInput) {
+//     console.log('You need to fill out the form!');
+//     return;
+//   }
+
+//   printGuestData(nameInput, commentInput);
+
+//   // reset form
+//   nameInputEl.val('');
+//   commentInputEl.val('');
+// };
+
+// formEl.on('submit', handleFormSubmit);
