d980b88f2ccd9ca78fc667f5fe5dc67d6db5ef94
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-04-04 19:30:38 -0500

    deleted script

--- assets/js/second.js
+++ assets/js/second.js
@@ -24,35 +24,54 @@ function handleAddTask() {
     taskdescription: description,
     status: 'todo',
   };
-  let tasklist = JSON.parse(localStorage.getItem("mytasks")) || []
+  let mytasks = JSON.parse(localStorage.getItem("mytasks")) || [];
   mytasks.push(newTask);
   localStorage.setItem("mytasks", JSON.stringify(mytasks));
+  renderTaskList() 
 }
 
 function renderTaskList() {
-  let tasklist = JSON.parse(localStorage.getItem(mytasks))
+  const tasklist =  JSON.parse(localStorage.getItem("mytasks")) || [];
   let toDo = $('#todo');
   let inProg = $('#in-progress');
   let done = $('#done');
-
-  toDo.empty();
-  inProg.empty();
-  done.empty();
-
+  console.log(toDo, inProg, done)
+  // toDo.empty();
+  // inProg.empty();
+  // done.empty();
+  
   tasklist.forEach((task) => {
+    console.log(task)
     let taskcard;
     if (task.status === "done") {
       taskcard = createTaskCard(task);
-      done.append(taskcard).addClass('todo');
+      done.append(taskcard).addClass('done');
     } else if (task.status === "in-progress") {
       taskcard = createTaskCard(task);
-      inProg.append(taskcard).addClass("todo");
+      inProg.append(taskcard).addClass('in-progress');
     } else {
       taskcard = createTaskCard(task);
       toDo.append(taskcard);
     }
   });
 }
+
+$('.draggable').draggable({
+  opacity: 0.7,
+  zIndex: 100,
+  // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+  helper: function (e) {
+    // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+    const original = $(e.target).hasClass('ui-draggable')
+      ? $(e.target)
+      : $(e.target).closest('.ui-draggable');
+    // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+    return original.clone().css({
+      width: original.outerWidth(),
+    });
+  },
+});
+
 
 function createTaskCard(task) {
   const taskCard = $('<div>')
@@ -67,38 +86,61 @@ function createTaskCard(task) {
     .addClass('btn btn-danger delete')
     .text('Delete')
     .attr('data-task-id', task.id);
-  cardDeleteBtn.on('click', handleDeleteTask);
+  cardDeleteBtn.on('click', handleDeleteTask());
 
+    // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+    if (task.dueDate && task.status !== 'done') {
+      const now = dayjs();
+      const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+  
+      // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+      if (now.isSame(taskDueDate, 'day')) {
+        taskCard.addClass('bg-warning text-white');
+      } else if (now.isAfter(taskDueDate)) {
+        taskCard.addClass('bg-danger text-white');
+        cardDeleteBtn.addClass('border-light');
+      }
+    }
   cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
   taskCard.append(cardHeader, cardBody);
 
-  if (task.taskdescription.toLowerCase() === "pastdue") {
-    taskCard.addClass("pastdue");
-  }
-  if (task.taskdescription.toLowerCase() === "duetoday") {
-    taskCard.addClass("duetoday");
-  }
   return taskCard;
-
-}
-
-// $(".card project-card draggable my-3").droppable({
-//   accept: ".project-card",
-//   drop: function (event, ui) {
-//     $(this).append(ui.draggable);
-//   }
-// });
-// $('.taskcard draggable my-3').droppable({
-//   accept: '.draggable',
-// });
-
-
 
 // function handleDeleteTask() {
 //   const taskId = $(this).attr('data-task-id');
 //   mytasks = mytasks.filter(task => task.id !== taskId);
 //   localStorage.setItem("mytasks", JSON.stringify(mytasks));
 // }
+
+}
+
+
+
+  // ? Remove project from the array. There is a method called `filter()` for this that is better suited which we will go over in a later activity. For now, we will use a `forEach()` loop to remove the project.
+//   tasks.forEach((task) => {
+//     if (task.id === taskId) {
+//       tasks.splice(tasks.indexOf(task), 1);
+//     }
+//   });
+
+//   // ? We will use our helper function to save the projects to localStorage
+//   saveTaskssToStorage(tasks);
+
+//   // ? Here we use our other function to print projects back to the screen
+//   printTaskLists();
+// }
+
+
+
+  // if (task.taskdescription.toLowerCase() === "pastdue") {
+  //   taskCard.addClass("pastdue");
+  // }
+  // if (task.taskdescription.toLowerCase() === "duetoday") {
+  // askCard.addClass("duetoday");
+
+
+
+
 
 
 // function displayProjects() {
